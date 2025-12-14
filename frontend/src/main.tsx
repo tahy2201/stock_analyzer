419d80ff31131c0531633bdc8a576516fb012a24
tahy2201 <hyg121arx8@gmail.com>
2025-12-14 11:43:33 +0900

    Set app title per environment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const appTitle = import.meta.env.MODE === 'production' ? 'stock-analyzer' : '(dev) stock-analyzer'
+document.title = appTitle
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
